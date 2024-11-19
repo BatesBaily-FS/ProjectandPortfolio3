ee079127f2a085ec307d84ed543ca5af6a156645
Baily Bates <BnBates1@student.fullsail.edu>
2024-11-18 20:40:54 -0600

    updated authJWT

--- api/middlewares/authJWT.js
+++ api/middlewares/authJWT.js
@@ -3,19 +3,19 @@ const JWTModel = require("../models/jwtSchema");
 
 const authJWT = async (req, res, next) => {
   const token = req.cookies.jwt || req.headers.authorization?.split(" ")[1];
-};
 
-const tokenRecord = await JWTModel.findOne({ token });
-if (!tokenRecord) {
-  return res.status(401).send("Unauthorized: Token not found");
-}
-
-jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
-  if (err) {
-    return res.status(403).send("Token is not valid");
+  const tokenRecord = await JWTModel.findOne({ token });
+  if (!tokenRecord) {
+    return res.status(401).send("Unauthorized: Token not found");
   }
-  req.user = user;
-  next();
-});
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err) {
+      return res.status(403).send("Token is not valid");
+    }
+    req.user = user;
+    next();
+  });
+};
 
 module.exports = authJWT;
