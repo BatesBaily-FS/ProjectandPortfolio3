d5f28d769035e89362d59a329a19913d7b1d61b0
Baily Bates <BnBates1@student.fullsail.edu>
2024-11-27 15:52:39 -0600

    update to heroku

--- api/routes/spotify.js
+++ api/routes/spotify.js
@@ -5,68 +5,14 @@ const authJWT = require("../middlewares/authJWT");
 
 router.use(authJWT);
 
-const isTokenExpired = (accessToken) => {
-  console.log("Access token is:", accessToken);
-  if (!accessToken) return true;
-  const tokenParts = accessToken.split(".");
-  if (tokenParts.length !== 3) {
-    console.error("Invalid token format");
-    return true;
-  }
-  try {
-    const payload = JSON.parse(
-      Buffer.from(accessToken.split(".")[1], "base64").toString()
-    );
-    const currentTime = Math.floor(Date.now() / 1000);
-    return payload.exp < currentTime;
-  } catch (error) {
-    console.error("Failed to parse access token payload", error);
-    return true;
-  }
-};
-
-const refreshAccessToken = async (refreshToken) => {
-  const client_id = process.env.SPOTIFY_CLIENT_ID;
-  const client_secret = process.env.SPOTIFY_CLIENT_SECRET;
-
-  try {
-    const response = await axios.post(
-      "https://accounts.spotify.com/api/token",
-      null,
-      {
-        params: {
-          grant_types: "refresh_token",
-          refresh_token: refreshToken,
-          client_id: client_id,
-          client_secret: client_secret,
-        },
-        headers: {
-          "Content-Type": "application/x-www-form-urlencoded",
-        },
-      }
-    );
-    return response.data.access_token;
-  } catch (error) {
-    console.error("Error refreshing access token", error);
-    throw new Error("Could not refresh accesss token");
-  }
-};
-
 router.get("/search", async (req, res) => {
   const { query } = req.query;
-  console.log("Request user", req.user);
 
   if (!req.user || !req.user.accessToken) {
     return res.status(401).send("Access Token is missing");
   }
 
   try {
-    if (isTokenExpired(req.user.accessToken)) {
-      console.log("Access token expired, refreshing...");
-      const newAccessToken = await refreshAccessToken(req.user.refreshToken);
-      req.user.accessToken = newAccessToken;
-    }
-
     const response = await axios.get(`https://api.spotify.com/v1/search`, {
       headers: {
         Authorization: `Bearer ${req.user.accessToken}`,
@@ -76,6 +22,7 @@ router.get("/search", async (req, res) => {
         type: "tracks,album,artist",
       },
     });
+
     res.json(response.data);
   } catch (error) {
     console.error(error);
