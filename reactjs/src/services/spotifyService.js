e0f4180d831becba80e11f7e26344189216636bf
Baily Bates <BnBates1@student.fullsail.edu>
2024-11-27 15:08:33 -0600

    update to heroku

--- reactjs/src/services/spotifyService.js
+++ reactjs/src/services/spotifyService.js
@@ -1,21 +1,28 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:3000/api";
+const API_URL =
+  process.env.NODE_ENV === "production"
+    ? "https://projectportfolio3-d62d160438fd.herokuapp.com/api"
+    : "http://localhost:3000/api";
 
 export const searchMusic = async (query) => {
   try {
-    const token = localStorage.getItem("accessToken");
+    const accessToken = localStorage.getItem("accessToken");
 
-    if (!token) {
+    if (!accessToken) {
       throw new Error("No access token found");
     }
 
     const response = await axios.get(`${API_URL}/search`, {
-      params: { query, type: "track,album,artist" },
       headers: {
-        Authorization: `Bearer ${token}`,
+        Authorization: `Bearer ${accessToken}`,
+      },
+      params: {
+        query,
+        type: "track,album,artist",
       },
     });
+
     return response.data;
   } catch (error) {
     console.error("Error fetching search results:", error);
