a8f4c413dfdccf9dc6aff170b5075b947ab01cb6
Baily Bates <BnBates1@student.fullsail.edu>
2024-11-27 22:14:34 -0600

    adding more styling elements to all pages

--- reactjs/src/components/Header.js
+++ reactjs/src/components/Header.js
@@ -9,21 +9,6 @@ function Header() {
       <div className="logo-container">
         <img src={logo} className="logo" />
       </div>
-      <div>
-        <nav>
-          <ul className="navigation">
-            <li>
-              <Link to="/noresults">No Results</Link>
-            </li>
-            <li>
-              <Link to="/results">Results</Link>
-            </li>
-            <li>
-              <Link to="/login">Login</Link>
-            </li>
-          </ul>
-        </nav>
-      </div>
       {(location.pathname === "/noresults" ||
         location.pathname === "/results") && <SearchComponent />}
     </header>
